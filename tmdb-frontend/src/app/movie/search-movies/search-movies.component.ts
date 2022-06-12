1bd83b395975f1a5f60261a8a3eeec6747512ced
CINNOVA\hqayyum <hqayyum@cinnova.com>
2022-06-12 18:49:45 +0500

    Frontend fixes

--- tmdb-frontend/src/app/movie/search-movies/search-movies.component.ts
+++ tmdb-frontend/src/app/movie/search-movies/search-movies.component.ts
@@ -1,13 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { MovieService } from 'app/services/movie.service';
-import { notification } from 'app/general-services/notification.model';
-import { NotificationService } from 'app/general-services/notification.service';
 
 @Component({
   selector: 'app-search-movies',
   templateUrl: './search-movies.component.html',
-  styleUrls: ['./search-movies.component.scss']
+  styleUrls: ['./search-movies.component.css']
 })
 export class SearchMoviesComponent implements OnInit {
 
@@ -17,55 +15,27 @@ export class SearchMoviesComponent implements OnInit {
   displayedColumns = ['PosterImage', 'Title', 'Rating', 'ReleaseDate'];
   searchKeyWord: string = null;
   pageNumber: number = 1;
-  input: boolean = true;
-
+  isSearched: boolean = false;
   constructor(private _movieService: MovieService,
-    private _notificationService: NotificationService,
+    private route: ActivatedRoute,
     private router: Router) { }
 
   ngOnInit(): void {
-    this._movieService.GetMoviesGenreList().subscribe((response) => { this.genreList = response.genreList.genres }, (err) => {
-      let notificationObj: notification = {
-        message: err.message,
-        type: "warning",
-      };
-      this._notificationService.open(notificationObj);
-    });
+    this._movieService.GetMoviesGenreList().subscribe((response) => { this.genreList = response.genreList.genres });
   }
 
   refreshList() {
-
-    this.moviesList = null;
+    this.isSearched = true;
 
     if (this.searchKeyWord != null && this.searchKeyWord != "") {
-      this.input = false;
       this._movieService.GetPaginatedMoviesListWithSearch(this.searchKeyWord, this.genreId, this.pageNumber).subscribe((response) => {
         this.moviesList = response.movieList;
-        this.input = true;
-      }, (err) => {
-        this.input = true;
-        let notificationObj: notification = {
-          message: err.message,
-          type: "warning",
-        };
-        this._notificationService.open(notificationObj);
       })
     }
     else {
-      if (this.genreId != 0 && this.genreId != null) {
-        this.input = false;
-        this._movieService.GetPaginatedMoviesListByGenre(this.genreId, this.pageNumber).subscribe((response) => {
-          this.moviesList = response.movieList;
-          this.input = true;
-        }, (err) => {
-          this.input = true;
-          let notificationObj: notification = {
-            message: err.message,
-            type: "warning",
-          };
-          this._notificationService.open(notificationObj);
-        })
-      }
+      this._movieService.GetPaginatedMoviesListByGenre(this.genreId, this.pageNumber).subscribe((response) => {
+        this.moviesList = response.movieList;
+      })
     }
   }
 
@@ -76,7 +46,9 @@ export class SearchMoviesComponent implements OnInit {
 
   setGenreId(event) {
     this.genreId = event.value;
-    this.refreshList();
+    if (this.genreId != 0 && this.genreId != null) {
+      this.refreshList();
+    }
     if (this.moviesList && (this.genreId == 0 || this.genreId == null)) {
       this.moviesList.total_results = 0;
       this.moviesList.results = [];
