0d29618703e418b46d303f7f26221113883a7a1a
CINNOVA\hqayyum <hqayyum@cinnova.com>
2022-06-17 17:44:53 +0500

    code cleaning

--- Backend-API/tmdbapi/ViewModels/MovieVIewModels.cs
+++ Backend-API/tmdbapi/ViewModels/MovieVIewModels.cs
@@ -22,13 +22,7 @@ namespace tmdbapi.ViewModels
         public string? release_date { get; set; }
         public List<int>? genre_ids { get; set; }
         public int id { get; set; }
-        //public string? original_title { get; set; }
-        //public string? original_language { get; set; }
         public string? title { get; set; }
-        //public string? backdrop_path { get; set; }
-        //public double popularity { get; set; }
-        //public int vote_count { get; set; }
-        //public bool video { get; set; }
         public double vote_average { get; set; }
     }
     public class MovieViewModel : IResponse
@@ -40,31 +34,14 @@ namespace tmdbapi.ViewModels
 
     public class MovieDetailsViewModel
     {
-        //public bool adult { get; set; }
-        //public string? backdrop_path { get; set; }
-        //public object? belongs_to_collection { get; set; }
-        //public int budget { get; set; }
         public List<GenreViewModel>? genres { get; set; }
-        //public string? homepage { get; set; }
         public int id { get; set; }
-        //public string? imdb_id { get; set; }
-        //public string? original_language { get; set; }
-        //public string? original_title { get; set; }
         public string? overview { get; set; }
         public double popularity { get; set; }
         public string? poster_path { get; set; }
-        //public List<ProductionCompany>? production_companies { get; set; }
-        //public List<ProductionCountry>? production_countries { get; set; }
         public string? release_date { get; set; }
-        //public int revenue { get; set; }
-        //public int runtime { get; set; }
-        //public List<SpokenLanguage>? spoken_languages { get; set; }
-        //public string? status { get; set; }
-        //public string? tagline { get; set; }
         public string? title { get; set; }
-        //public bool video { get; set; }
         public double vote_average { get; set; }
-        //public int vote_count { get; set; }
     }
 
     public class MovieCastViewModel
@@ -75,18 +52,8 @@ namespace tmdbapi.ViewModels
 
     public class CastDetailsViewModel
     {
-        //public bool adult { get; set; }
-        //public int gender { get; set; }
         public int id { get; set; }
-        //public string? known_for_department { get; set; }
         public string? name { get; set; }
-        //public string? original_name { get; set; }
-        //public double popularity { get; set; }
-        //public string? profile_path { get; set; }
-        //public int cast_id { get; set; }
-        //public string? character { get; set; }
-        //public string? credit_id { get; set; }
-        //public int order { get; set; }
     }
     public class GenreViewModel
     {
